8ac180b7dc3ae714358a35c0ef243e5e9a03d849
Alejandro Raya <alejandroraya09@hotmail.com>
2024-08-03 01:14:58 -0600

    Variables, Impresion y Salida de datos

---
+++ MiPrimerProyecto/Program.cs
@@ -0,0 +1,47 @@
+﻿//VARIABLES
+/*
+Console.WriteLine("Hello, World!");
+
+
+bool esRojo;
+bool esGuitarra;
+
+esRojo = false; 
+esGuitarra = false;
+
+int Edad = 12;
+int edadPromedio = 18;
+
+float peso = 45.7f;
+
+double temp = 651.87825;
+
+
+char letra = 'A';
+char letra2 = 'B';
+
+string palabra = "Buenas noches";
+
+Console.WriteLine(esRojo);
+Console.WriteLine(esGuitarra);
+Console.WriteLine(Edad);
+Console.WriteLine(edadPromedio);
+Console.WriteLine(peso);
+Console.WriteLine(temp);
+Console.WriteLine(letra);
+Console.WriteLine(letra2);
+Console.WriteLine(palabra);
+*/
+
+
+//IMPRESION Y ENTRADA DE DATOS
+
+Console.WriteLine("Bienvenido");
+Console.Write("Ingresa Tu Nombre: ");
+string name = Console.ReadLine();
+
+
+Console.Write("Ingrese su edad: ");
+string edad = Console.ReadLine();
+
+Console.WriteLine("Hola " + name + "Tu edad es: " + edad);
