ae0e9280a9fff33a435b54468f1ecb12fe6c3719
Alejandro Raya <alejandroraya09@hotmail.com>
2024-08-03 01:28:42 -0600

    Operaciones matematicas

--- MiPrimerProyecto/Program.cs
+++ MiPrimerProyecto/Program.cs
@@ -33,15 +33,57 @@ Console.WriteLine(letra2);
 Console.WriteLine(palabra);
 */
 
-
 //IMPRESION Y ENTRADA DE DATOS
-
+/*
 Console.WriteLine("Bienvenido");
 Console.Write("Ingresa Tu Nombre: ");
 string name = Console.ReadLine();
 
-
 Console.Write("Ingrese su edad: ");
-string edad = Console.ReadLine();
+int edad = Int32.Parse(Console.ReadLine());
 
-Console.WriteLine("Hola " + name + "Tu edad es: " + edad);
+Console.Write("Ingrese su Peso: ");
+float peso = float.Parse(Console.ReadLine());
+
+Console.Write("Ingrese su Temperatura: ");
+double temp = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("Hola " + name + 
+    ", Tu edad es: " + edad + 
+    ", Tu peso es: " + peso + 
+    ", Su temperatura es ... " + temp);
+*/
+
+//VAR
+/*
+var x = 21;
+var y = "Variable String";
+var z = 45.98;
+var w = true;
+*/
+
+//OPERACIONES MATEMATICAS
+Console.Write("Ingrese su primer Numero :");
+int a = Int32.Parse(Console.ReadLine());
+
+Console.Write("Ingrese su segundo Numero :");
+int b = Int32.Parse(Console.ReadLine());
+
+//SUMA
+int suma = a + b;
+//RESTA
+int resta = a - b;
+//MULTIPLICACION 
+int multiplicacion = a * b;
+//DIVISION
+float division = a / b;
+//MODULO
+int modulo = a % b;
+
+
+Console.WriteLine("La Suma es: " + suma);
+Console.WriteLine("La Resta es: " + resta);
+Console.WriteLine("La Multiplicacion es: " + multiplicacion);
+Console.WriteLine("La Divicion es: " + division);
+Console.WriteLine("La Modulo es: " + modulo);
